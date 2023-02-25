c21312e5c8dd9d544f59a9ca3758e1c21c400c68
Bhola Kumar <87958764+Bhola-kumar@users.noreply.github.com>
2023-02-25 19:53:30 +0530

    version 1

---
+++ script.js
@@ -0,0 +1,9 @@
+// for login button
+function signupfn(){ 
+  window.location.href = "http://localhost/proj/signup.php";
+}
+  
+// for login button
+function loginfn(){
+  window.location.href = "http://localhost/proj/login.php";
+}
